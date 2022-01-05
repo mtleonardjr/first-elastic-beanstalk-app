a92f482d5775e0e1fb929aacad2cd538bcaa4cb2
Mike Leonard <mtleonardjr@gmail.com>
2022-01-05 15:42:59 -0500

    updated version

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the homepage');
+});
+
+const port = process.env.port || 3000;
+app.listen(port, () => {
+    console.log('listening on port: ' + port);
+});

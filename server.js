a0934ab95ced9cdcaf416c708357eef4398a4188
Mike Leonard <mtleonardjr@gmail.com>
2022-01-05 16:26:11 -0500

    updated logging message

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the homepage');
+    res.send('Welcome to the updated homepage');
 });
 
 const port = process.env.port || 3000;

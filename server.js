d243e3c6db243761b5316d6209e0eaabbfbb715a
Mike Leonard <mtleonardjr@gmail.com>
2022-01-06 13:19:35 -0500

    added html to homepage

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const http = require('http');
+const fs = require('fs');
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the updated homepage');
+    res.writeHead(200, { 'content-type': 'text/html' })
+    fs.createReadStream('index.html').pipe(res)
 });
 
 const port = process.env.port || 3000;
